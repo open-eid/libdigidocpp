d6c29be091901770c555891cecb57250db851a17
Raul Metsma <raul@metsma.ee>
2018-10-24 09:54:21 +0300

    Update Xerces-C to 3.2.2 (#237)
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- prepare_osx_build_environment.sh
+++ prepare_osx_build_environment.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -e
 
-XERCES_DIR=xerces-c-3.2.1
+XERCES_DIR=xerces-c-3.2.2
 XMLSEC_DIR=xml-security-c-2.0.1
 XSD=xsd-4.0.0-i686-macosx
 OPENSSL_DIR=openssl-1.0.2p
 #OPENSSL_DIR=openssl-1.1.0i
 LIBXML2_DIR=libxml2-2.9.8
 ANDROID_NDK=android-ndk-r14b
-FREETYPE_DIR=freetype-2.9
-FONTCONFIG_DIR=fontconfig-2.12.6
+FREETYPE_DIR=freetype-2.9.1
+FONTCONFIG_DIR=fontconfig-2.13.1
 PODOFO_DIR=podofo-0.9.4
 ARGS="$@"
 
