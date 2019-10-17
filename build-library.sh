df02b7635a3bd5b56b9bfa6043e045d20d22c962
Raul Metsma <raul@metsma.ee>
2019-10-17 14:51:11 +0300

    Merge build scripts
    IB-6055
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

---
+++ build-library.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+  echo "Usage:"
+  echo "  $0 target [maketask]"
+  echo "  target: osx ios iossimulator androidarm androidarm64 androidx86"
+  echo "To control iOS, macOS builds set environment variables:"
+  echo " minimum deployment target"
+  echo " - MACOSX_DEPLOYMENT_TARGET=10.11"
+  echo " - IPHONEOS_DEPLOYMENT_TARGET=9.0"
+  echo " archs to build on iOS"
+  echo " - ARCHS=\"armv7 arm64\" (iOS)"
+  echo " - ARCHS=\"x86_64\" (iPhoneSimulator)"
+  exit
+fi
+
+case "$@" in
+*android*)
+  case "$@" in
+  *x86*)
+    TARGET=androidx86
+    ARCH="x86"
+    ;;
+  *arm64*)
+    TARGET=androidarm64
+    ARCH="arm64-v8a"
+    ;;
+  *)
+    TARGET=androidarm
+    ARCH="armeabi-v7a"
+    ;;
+  esac
+  TARGET_PATH=/Library/libdigidocpp.${TARGET}
+  CMAKEARGS="
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_ANDROID_STANDALONE_TOOLCHAIN=${TARGET_PATH} \
+    -DCMAKE_ANDROID_ARCH_ABI=${ARCH} \
+    -DCMAKE_C_FLAGS='-DIOAPI_NO_64' \
+    -DCMAKE_CXX_FLAGS='-Oz' \
+    -DBoost_INCLUDE_DIR=NOTFOUND \
+    -DDOXYGEN_EXECUTABLE=NOTFOUND \
+    -DBUILD_TOOLS=NO \
+    -DBUILD_SHARED_LIBS=NO"
+  ;;
+*ios*)
+  case "$@" in
+  *simulator*)
+    echo "Building for iOS Simulator"
+    TARGET=iphonesimulator
+    : ${ARCHS:="x86_64"}
+    ;;
+  *)
+    echo "Building for iOS"
+    TARGET=iphoneos
+    : ${ARCHS:="armv7 arm64"}
+    ;;
+  esac
+  TARGET_PATH=/Library/libdigidocpp.${TARGET}
+  : ${IPHONEOS_DEPLOYMENT_TARGET:="9.0"}
+  export IPHONEOS_DEPLOYMENT_TARGET
+  CMAKEARGS="
+    -DCMAKE_C_COMPILER_WORKS=yes \
+    -DCMAKE_CXX_COMPILER_WORKS=yes \
+    -DCMAKE_OSX_SYSROOT=${TARGET} \
+    -DCMAKE_OSX_ARCHITECTURES='${ARCHS// /;}' \
+    -DIOS=YES \
+    -DFRAMEWORK=off \
+    -DUSE_KEYCHAIN=off \
+    -DSWIG_EXECUTABLE=NOTFOUND \
+    -DBoost_INCLUDE_DIR=NOTFOUND \
+    -DDOXYGEN_EXECUTABLE=NOTFOUND \
+    -DBUILD_TOOLS=NO \
+    -DBUILD_SHARED_LIBS=NO"
+  ;;
+*)
+  echo "Building for macOS"
+  TARGET=macOS
+  TARGET_PATH=/Library/libdigidocpp
+  : ${ARCHS:="x86_64"}
+  : ${MACOSX_DEPLOYMENT_TARGET:="10.11"}
+  export MACOSX_DEPLOYMENT_TARGET
+esac
+
+rm -rf ${TARGET}
+mkdir -p ${TARGET}
+cd ${TARGET}
+cmake \
+    -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
+    -DCMAKE_INSTALL_PREFIX=${TARGET_PATH} \
+    -DOPENSSL_ROOT_DIR=${TARGET_PATH} \
+    -DXercesC_ROOT=${TARGET_PATH} \
+    ${CMAKEARGS} \
+    ..
+sudo make ${@:2}
+cd ..
--- examples/README.md
+++ examples/README.md
@@ -3,7 +3,7 @@
 Contents of this folder is provided on as is terms, whithout support.
 
 * [DigiDocCSharp](./DigiDocCSharp/README.md) - example project to use C# on Windows
-* [libdigidocpp-android](./libdigidocpp-android/README.md) - example project to use on Android platform
-* [libdigidocpp-ios](./libdigidocpp-ios/README.md) - example project to use on iOS platform
-* [libdigidocpp-java](./libdigidocpp-java/README.md) - example project to use with java
+* [android](./android/README.md) - example project to use on Android platform
+* [ios](./ios/README.md) - example project to use on iOS platform
+* [java](./java/README.md) - example project to use with java
 * [pkcs11sign.cpp](pkcs11sign.cpp) - Simple PKCS11Signer example
