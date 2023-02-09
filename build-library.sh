485e8841b31d46fb5e588debdc77538694f1fa2f
Raul Metsma <raul@metsma.ee>
2023-02-09 15:26:43 +0200

    Update OpenSSL 1.1.1t (#525)
    IB-7656
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- build-library.sh
+++ build-library.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$#" -eq 0 ]; then
   echo "Usage:"
   echo "  $0 target [maketask]"
-  echo "  target: osx ios iossimulator androidarm androidarm64 androidx86 androidx86_64"
+  echo "  target: osx ios iossimulator ioscatalyst androidarm androidarm64 androidx86 androidx86_64"
   echo "To control iOS, macOS builds set environment variables:"
   echo " minimum deployment target"
   echo " - MACOSX_DEPLOYMENT_TARGET=10.15"
