12655e1455daf99da587d6ef938e6f98c76e7ebc
Raul Metsma <raul@metsma.ee>
2024-01-24 08:47:52 +0200

    Use XSD 4.2.0 (#565)
    IB-7856
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- prepare_win_build_environment.ps1
+++ prepare_win_build_environment.ps1
@@ -2,24 +2,20 @@
 param(
 	[string]$vcpkg = "vcpkg\vcpkg.exe",
 	[string]$git = "git.exe",
-	[string]$7zip = "C:\Program Files\7-Zip\7z.exe",
 	[string]$toolset = "142",
-	[string]$xsdver = "xsd-4.0.0-i686-windows",
 	[switch]$xsd = $false,
 	[switch]$dependencies = $false
 )
 
-if(!(Test-Path -Path $vcpkg)) {
-	$vcpkg_dir = (split-path -parent $vcpkg)
-	& $git clone --depth 1 https://github.com/microsoft/vcpkg $vcpkg_dir
-	& $vcpkg_dir\bootstrap-vcpkg.bat
-}
-
 function xsd() {
 	$client = new-object System.Net.WebClient
-	$client.DownloadFile("http://www.codesynthesis.com/download/xsd/4.0/windows/i686/$xsdver.zip", "$PSScriptRoot\$xsdver.zip")
-	& $7zip x "$xsdver.zip" > $null
-	Rename-Item $xsdver xsd
+	& mkdir xsd
+	foreach($xsdver in @("xsd-4.2.0-x86_64-windows10", "libxsd-4.2.0-windows")) {
+		$client.DownloadFile("https://www.codesynthesis.com/download/xsd/4.2/windows/windows10/x86_64/$xsdver.zip", "$PSScriptRoot\$xsdver.zip")
+		& tar xf "$xsdver.zip"
+		& xcopy /e /r /y $xsdver\*.* xsd
+		& Remove-Item $xsdver -Force -Recurse -ErrorAction Ignore
+	}
 }
 
 if($xsd) {
@@ -27,6 +23,11 @@ if($xsd) {
 }
 
 if($dependencies) {
+	if(!(Test-Path -Path $vcpkg)) {
+		$vcpkg_dir = (split-path -parent $vcpkg)
+		& $git clone --depth 1 https://github.com/microsoft/vcpkg $vcpkg_dir
+		& $vcpkg_dir\bootstrap-vcpkg.bat
+	}
 	& $vcpkg install --clean-after-build --triplet x86-windows-v$toolset --x-feature=tests --x-install-root=vcpkg_installed_x86
 	& $vcpkg install --clean-after-build --triplet x64-windows-v$toolset --x-feature=tests --x-install-root=vcpkg_installed_x64
 }
