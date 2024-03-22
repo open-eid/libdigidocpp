79735748ec1474571b4a33d8b6ceb0f092a186c2
kristelmerilain <kristelmerilain@users.noreply.github.com>
2024-03-22 07:40:47 +0000

    deploy: 994a25114624dc997627e537113127cffd21ab97

---
+++ structdigidoc_1_1ContainerOpenCB.js
@@ -0,0 +1,5 @@
+var structdigidoc_1_1ContainerOpenCB =
+[
+    [ "~ContainerOpenCB", "structdigidoc_1_1ContainerOpenCB.html#a3dc631e2615cfc7a182855f6e2cf849d", null ],
+    [ "validateOnline", "structdigidoc_1_1ContainerOpenCB.html#a6a02d06df9d790cd5e5b792ea8a1fa65", null ]
+];
