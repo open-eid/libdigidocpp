b43b40c757ca6a2c6081d3148cdb42f3363ea679
kristelmerilain <kristelmerilain@users.noreply.github.com>
2024-12-03 10:14:21 +0000

    deploy: e7bbc3030000136dd7d3a3ec821a8f47c1e814d4

---
+++ classdigidoc_1_1DataFile.js
@@ -0,0 +1,12 @@
+var classdigidoc_1_1DataFile =
+[
+    [ "~DataFile", "classdigidoc_1_1DataFile.html#a8717582401bcbb28a57af9bcf0788fdb", null ],
+    [ "DataFile", "classdigidoc_1_1DataFile.html#a89f1d187ae88cd7f921793e7e672c7d4", null ],
+    [ "calcDigest", "classdigidoc_1_1DataFile.html#a6b5696a0f7c9f1ef9507889c1e34a348", null ],
+    [ "fileName", "classdigidoc_1_1DataFile.html#a65846794c66f0e07a91e2933d809355e", null ],
+    [ "fileSize", "classdigidoc_1_1DataFile.html#a2cd791213717bd74b594b84c5789dfd4", null ],
+    [ "id", "classdigidoc_1_1DataFile.html#a74ecdb44ac917ded6331b17a377b472d", null ],
+    [ "mediaType", "classdigidoc_1_1DataFile.html#aaa3ed6dfebfadd3e0bdc016039251184", null ],
+    [ "saveAs", "classdigidoc_1_1DataFile.html#aa6ece28115b28ce76af231cfc6c9b37d", null ],
+    [ "saveAs", "classdigidoc_1_1DataFile.html#a1a1499e6a88783152cceca8ff1b54ffc", null ]
+];
