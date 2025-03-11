a905f5611d1a8ae85bf01748331eb3637ed569ee
kristelmerilain <kristelmerilain@users.noreply.github.com>
2025-03-11 07:47:58 +0000

    deploy: 935120298f690db058bb1dd29dd342eb0deb6cf4

---
+++ classdigidoc_1_1PKCS11Signer.js
@@ -0,0 +1,11 @@
+var classdigidoc_1_1PKCS11Signer =
+[
+    [ "PKCS11Signer", "classdigidoc_1_1PKCS11Signer.html#a135e22209cf3874e9398be704c56eba8", null ],
+    [ "~PKCS11Signer", "classdigidoc_1_1PKCS11Signer.html#a2f2fc7aac3a01a637da0b9e6f4198dec", null ],
+    [ "cert", "classdigidoc_1_1PKCS11Signer.html#a6ecbf0e36594d5f8a073080a7f1ab3ca", null ],
+    [ "method", "classdigidoc_1_1PKCS11Signer.html#a67763564693fd121fd475c644685642a", null ],
+    [ "pin", "classdigidoc_1_1PKCS11Signer.html#a99d922f4fd3def7c3c385a46f406a279", null ],
+    [ "selectSigningCertificate", "classdigidoc_1_1PKCS11Signer.html#a1481fcce312f45751b4c8a2253c52201", null ],
+    [ "setPin", "classdigidoc_1_1PKCS11Signer.html#ab51b4c5707f29989c85b9fb883336e82", null ],
+    [ "sign", "classdigidoc_1_1PKCS11Signer.html#afc1bf7b043313a947072b8af7cba5891", null ]
+];
