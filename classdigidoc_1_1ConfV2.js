ec0fb9eef5f802be5aa4031321a9fcc21c720c73
kristelmerilain <kristelmerilain@users.noreply.github.com>
2025-05-07 05:25:33 +0000

    deploy: 7de9baf9a13a947ee737aa5415f5e1a0a14a42d5

---
+++ classdigidoc_1_1ConfV2.js
@@ -0,0 +1,7 @@
+var classdigidoc_1_1ConfV2 =
+[
+    [ "ConfV2", "classdigidoc_1_1ConfV2.html#a9e33d5f2cd57315958bcab64d0638d14", null ],
+    [ "~ConfV2", "classdigidoc_1_1ConfV2.html#a0ca0269bee7dd5f3585e284a217946e0", null ],
+    [ "instance", "classdigidoc_1_1ConfV2.html#a675094090e1a85c66e7b7013816c09da", null ],
+    [ "verifyServiceCert", "classdigidoc_1_1ConfV2.html#a5376a9c52bc6de08d4ae087af233fc0f", null ]
+];
