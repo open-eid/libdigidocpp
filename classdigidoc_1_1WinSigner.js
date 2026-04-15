b013c68972a5cd2a37989ed5b3ea33d659f222e5
kristelmerilain <kristelmerilain@users.noreply.github.com>
2026-04-15 06:05:08 +0000

    deploy: 68ec5bf6a84d63a48dcfbc891fe1bf21fff4429e

---
+++ classdigidoc_1_1WinSigner.js
@@ -0,0 +1,8 @@
+var classdigidoc_1_1WinSigner =
+[
+    [ "WinSigner", "classdigidoc_1_1WinSigner.html#ac2c83b9af07a37bff88d175ae9273d8e", null ],
+    [ "~WinSigner", "classdigidoc_1_1WinSigner.html#a7247d0b14a1262ae7cc6e9b3718d8f9a", null ],
+    [ "setPin", "classdigidoc_1_1WinSigner.html#ab52be1e7ae19db47dd89a271fe492b4d", null ],
+    [ "setSelectFirst", "classdigidoc_1_1WinSigner.html#ad89fcd87b9426c30587183203ff3c6ab", null ],
+    [ "setThumbprint", "classdigidoc_1_1WinSigner.html#a39fa755eb67560a8f7295b9f4cf5fa97", null ]
+];
