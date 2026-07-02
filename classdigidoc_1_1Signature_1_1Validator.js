db59c7c9716749cc0827b2e12b9e08719a8986c9
Counter178 <Counter178@users.noreply.github.com>
2026-07-02 09:48:10 +0000

    deploy: 26a8ad37e9c066216ce15a135a4450ac1975129f

---
+++ classdigidoc_1_1Signature_1_1Validator.js
@@ -0,0 +1,16 @@
+var classdigidoc_1_1Signature_1_1Validator =
+[
+    [ "Status", "classdigidoc_1_1Signature_1_1Validator.html#af08d26ad92c6fbb6b000bceb1a417402", [
+      [ "Valid", "classdigidoc_1_1Signature_1_1Validator.html#af08d26ad92c6fbb6b000bceb1a417402a12f1aa55da12db51343b6c18c2771f38", null ],
+      [ "Warning", "classdigidoc_1_1Signature_1_1Validator.html#af08d26ad92c6fbb6b000bceb1a417402a2998872a87f1ea8abe2120dff8a00001", null ],
+      [ "NonQSCD", "classdigidoc_1_1Signature_1_1Validator.html#af08d26ad92c6fbb6b000bceb1a417402a6660c6ff9ef6e101f9e5c76509538c4b", null ],
+      [ "Test", "classdigidoc_1_1Signature_1_1Validator.html#af08d26ad92c6fbb6b000bceb1a417402a4a67d4e39691ed79ff76130c936fbb6c", null ],
+      [ "Invalid", "classdigidoc_1_1Signature_1_1Validator.html#af08d26ad92c6fbb6b000bceb1a417402a8fe41343431cd16eba1b782df3b92433", null ],
+      [ "Unknown", "classdigidoc_1_1Signature_1_1Validator.html#af08d26ad92c6fbb6b000bceb1a417402a893e8cb6116a25bf35e2480c18ba2c2c", null ]
+    ] ],
+    [ "Validator", "classdigidoc_1_1Signature_1_1Validator.html#a89d09c978b0e0e45728a6b32b4b3976e", null ],
+    [ "~Validator", "classdigidoc_1_1Signature_1_1Validator.html#a8982cf7f244ebe484f2230ae176e5e87", null ],
+    [ "diagnostics", "classdigidoc_1_1Signature_1_1Validator.html#a47f833a287de645688792b0e3c573e3e", null ],
+    [ "status", "classdigidoc_1_1Signature_1_1Validator.html#a8e7b5091a9d51763e582e74d7b8cde0e", null ],
+    [ "warnings", "classdigidoc_1_1Signature_1_1Validator.html#a66166d43cb461861d22354d2af135d17", null ]
+];
---
+++ hierarchy.js
@@ -0,0 +1,31 @@
+var hierarchy =
+[
+    [ "digidoc::Conf", "classdigidoc_1_1Conf.html", [
+      [ "digidoc::ConfV2", "classdigidoc_1_1ConfV2.html", [
+        [ "digidoc::ConfV3", "classdigidoc_1_1ConfV3.html", [
+          [ "digidoc::ConfV4", "classdigidoc_1_1ConfV4.html", [
+            [ "digidoc::ConfV5", "classdigidoc_1_1ConfV5.html", [
+              [ "digidoc::XmlConfV5", "classdigidoc_1_1XmlConfV5.html", null ]
+            ] ],
+            [ "digidoc::XmlConfV4", "classdigidoc_1_1XmlConfV4.html", null ]
+          ] ],
+          [ "digidoc::XmlConfV3", "classdigidoc_1_1XmlConfV3.html", null ]
+        ] ],
+        [ "digidoc::XmlConfV2", "classdigidoc_1_1XmlConfV2.html", null ]
+      ] ],
+      [ "digidoc::XmlConf", "classdigidoc_1_1XmlConf.html", null ]
+    ] ],
+    [ "digidoc::Container", "classdigidoc_1_1Container.html", null ],
+    [ "digidoc::ContainerOpenCB", "structdigidoc_1_1ContainerOpenCB.html", null ],
+    [ "digidoc::DataFile", "classdigidoc_1_1DataFile.html", null ],
+    [ "digidoc::Exception", "classdigidoc_1_1Exception.html", null ],
+    [ "digidoc::Signature", "classdigidoc_1_1Signature.html", null ],
+    [ "digidoc::Signer", "classdigidoc_1_1Signer.html", [
+      [ "digidoc::PKCS11Signer", "classdigidoc_1_1PKCS11Signer.html", null ],
+      [ "digidoc::PKCS12Signer", "classdigidoc_1_1PKCS12Signer.html", null ],
+      [ "digidoc::WinSigner", "classdigidoc_1_1WinSigner.html", null ]
+    ] ],
+    [ "digidoc::TSAInfo", "structdigidoc_1_1TSAInfo.html", null ],
+    [ "digidoc::Signature::Validator", "classdigidoc_1_1Signature_1_1Validator.html", null ],
+    [ "digidoc::X509Cert", "classdigidoc_1_1X509Cert.html", null ]
+];
